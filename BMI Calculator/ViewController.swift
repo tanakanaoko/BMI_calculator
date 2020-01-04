0dd74a5e3e343ef4c844abdab615258c74217581
tanakanaoko <nakochi.xxx@gmail.com>
2020-01-04 14:11:15 +0900

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by 田中奈穂子 on 2020/01/04.
+//  Copyright © 2020 tanaka naoko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
