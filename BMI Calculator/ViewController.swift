50570f842d4aea3e2cfeca56d8798e1ee59a5779
tanakanaoko <nakochi.xxx@gmail.com>
2020-01-05 09:33:16 +0900

    BMIが計算できる

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var heightTextgField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func calculateBMI(height: Double, weight: Double) -> Double {
+        return weight/(height*height)
+    }
 
-
+    @IBAction func botton_tapped(_ sender: Any) {
+        let heightText = heightTextgField.text
+        let weightText = weightTextField.text
+        
+        guard let height = heightText, let weight = weightText else {
+            return
+        }
+        
+        if let heightDouble = Double(height), let weightDouble = Double(weight) {
+            let bmi = calculateBMI(height: heightDouble, weight: weightDouble)
+            print(bmi)
+        }
+    }
+    
 }
 
